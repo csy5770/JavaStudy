4229f756a91f4134f181b0a1ca9526ede0263920
csy5770 <yush60@gmail.com>
2021-07-22 16:19:59 +0900

    20210722 study

---
+++ javastudy/src/javastudy/JavaStudy3.java
@@ -0,0 +1,17 @@
+package javastudy;
+
+import java.util.Scanner;
+
+public class JavaStudy3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();		
+		if(str.equalsIgnoreCase("CSY")) {
+			System.out.println("CHOI S Y");
+		}
+		
+		s.close();
+	}
+
+}
