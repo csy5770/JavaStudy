9364795d8aba9c37d1c8429510acd40018596fe1
csy5770 <yush60@gmail.com>
2021-07-22 16:32:04 +0900

    20210722 study2

---
+++ javastudy/src/javastudy/JavaStudy4.java
@@ -0,0 +1,16 @@
+package javastudy;
+
+import java.util.Scanner;
+
+public class JavaStudy4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		if(str.equals("a") || str.equals("A")) {
+			System.out.println("Apple");
+		}
+	}
+
+}
