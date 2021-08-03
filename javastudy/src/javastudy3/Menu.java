5bf1b869332372970abcaec22895c5d645facd5f
csy5770 <yush60@gmail.com>
2021-08-03 16:51:51 +0900

    20210803

--- javastudy/src/javastudy3/Menu.java
+++ javastudy/src/javastudy3/Menu.java
@@ -1,9 +1,11 @@
 package javastudy3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -32,16 +34,36 @@ public class Menu {
 	void addPrice(String price) {
 		this.alPrice.add(Integer.parseInt(price));
 	}
+	void changeMenu(int menu_num,String name, int price) {//set, arraylist 활용
+		this.alName.set(menu_num-1,name);
+		this.alPrice.set(menu_num-1,price);
+	}
+
+	void deleteMenu(int menu_num) {//remove arraylist 활용
+		this.alName.remove(menu_num-1);
+	}
 	void showMenu() { //alName, alPrice 출력.
 		for(int i=0; i<alName.size(); i++) {
-			System.out.println("메뉴명: "+ alName.get(i)+", "+"가격: "+ alPrice.get(i));
+			System.out.println((i+1)+"."+"메뉴명: "+ alName.get(i)+", "+"가격: "+ alPrice.get(i));
 		}	
 	}
-	void save() { //ArrayList alName과 alPrice를 menu.txt에 저장.
+	void save() throws IOException { //ArrayList alName과 alPrice를 menu.txt에 저장.
+		File file=new File("e:/menu.txt");
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 		
+		for (int i = 0; i < alName.size(); i++) {
+			final String name = alName.get(i);
+			final int price = alPrice.get(i);
+			
+			bw.write(name + "," + price); 
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
 	}
 	void load() {//menu.txt를 읽어서 ArrayList에 로드.
-		File file=new File("d:/menu.txt");
+		File file=new File("e:/menu.txt");
 		if(file.exists()) {
 			BufferedReader inFile = null;
 			try {
--- javastudy/src/javastudy3/MenuList2.java
+++ javastudy/src/javastudy3/MenuList2.java
@@ -1,16 +1,18 @@
 package javastudy3;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MenuList2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// m:메뉴 관리
 		// o:주문받기
 		// s:실적보기
 		// x:프로그램 종료.
 		Scanner s = new Scanner(System.in);
 		Menu menu = new Menu();
+		menu.load();
 		
 		while (true) {
 			// s.nextLine() 사용자한테 공백을 포함한 문자열을 입력받는 메서드(함수)
@@ -22,7 +24,61 @@ public class MenuList2 {
 				//main()함수에서의 return 은 프로그램 종료 
 			}
 			else if(str.equals("m")|| str.equals("M")) {
-				System.out.println("메뉴");
+				System.out.println("메뉴관리");
+				menu.showMenu();
+				System.out.println("메뉴 작업.(c:추가,r:목록보기,u:수정,d:삭제,q:종료)");
+				while (true) {
+					String st = s.nextLine();
+					if(st.equals("q")|| st.equals("Q")) {
+						System.out.println("CRUD 종료");
+						break;
+					}
+					else if(st.equals("c")|| st.equals("C")) {
+						System.out.println("추가할 메뉴 입력 ");
+						String mn = s.nextLine();
+						System.out.println("가격 입력");
+						int pr = s.nextInt();
+						s.nextLine();
+						menu.addName(mn);
+						menu.addPrice(pr);
+						System.out.println("메뉴에 추가 되었습니다.");
+						System.out.println("");
+					}
+					else if(st.equals("r")|| st.equals("R")) {
+						System.out.println("목록보기");
+						menu.showMenu();
+						System.out.println("");
+					}
+					else if(st.equals("u")|| st.equals("U")) {
+						System.out.println("수정할 메뉴번호");
+						int id = s.nextInt();//메뉴번호 스캐너
+						s.nextLine();
+						System.out.println("이 이름으로 변경합니다 ");
+						String nm = s.nextLine(); //변경할 메뉴 이름 스캐너
+						System.out.println("이 가격으로 변경합니다");
+						int npr = s.nextInt(); //변경할 가격 스캐너
+						s.nextLine();
+						menu.changeMenu(id,nm,npr);
+						System.out.println("메뉴 변경 완료");
+						System.out.println("");
+					}
+					else if(st.equals("d")|| st.equals("D")) {
+						System.out.println("삭제할 메뉴번호");
+						int id = s.nextInt();//삭제할 메뉴번호 스캐너
+						s.nextLine();
+						menu.deleteMenu(id);
+						System.out.println("삭제 했습니다.");
+						System.out.println("");
+					}
+					else {
+						System.out.println("잘못된 입력");
+						System.out.println("");
+					}
+					System.out.println("메뉴 작업.(c:추가,r:목록보기,u:수정,d:삭제)");
+					
+				}
+				
+				
 			}
 			else if(str.equals("o")|| str.equals("O")) {
 				System.out.println("주문");
@@ -35,6 +91,7 @@ public class MenuList2 {
 			}
 		
 		}
+		menu.save();
 	}
 
 }
